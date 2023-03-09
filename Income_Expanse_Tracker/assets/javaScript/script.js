f80aba98aa97aa70b11cbe6fea1b74cb980be7e1
karmugil <karmugilrc@gmail.com>
2023-03-09 21:25:13 +0530

    Update script.js
    if you want add it

--- Income_Expanse_Tracker/assets/javaScript/script.js
+++ Income_Expanse_Tracker/assets/javaScript/script.js
@@ -1,54 +1,85 @@
-const balance        =document.querySelector("#balance");
-const inc_amt        =document.querySelector("#inc-amt");
-const exp_amt        =document.querySelector("#exp-amt");
-const trans          =document.querySelector("#trans");
-const description    =document.querySelector("#desc");
-const amount         =document.querySelector("#amount");
-const form           =document.querySelector("#from");
-const dummyData=[
-    {id:1,description:"flower",amount:-20},
-    {id:2,description:"Salary",amount:300},
-    {id:3,description:"Book",amount:-10},
-    {id:4,description:"Camera",amount:150},
-    {id:5,description:"Salary",amount:300},
+// Select elements from the DOM
+const balance = document.querySelector("#balance");
+const inc_amt = document.querySelector("#inc-amt");
+const exp_amt = document.querySelector("#exp-amt");
+const trans = document.querySelector("#trans");
+const description = document.querySelector("#desc");
+const amount = document.querySelector("#amount");
+const form = document.querySelector("#form");
+
+// Dummy transaction data
+const dummyData = [
+  { id: 1, description: "Salary", amount: 300 },
+  { id: 2, description: "Rent", amount: -150 },
+  { id: 3, description: "Groceries", amount: -50 },
 ];
-let transaction=dummyData;
-// functions
-function loadTransactionDetails(transaction){
-  const sign=transaction.amount < 0 ? "-":"+";
-  const item=document.createElement("li");
-  item.classList.add(transaction.amount <0 ? "exp" : "inc")
-  item.innerHTML=`
-  ${transaction.description}
-  <span>${sign} ${Math.abs(transaction.amount)}<span>
-  <button class="btn-del" onclick="removeTrans( ${transaction.id} )">x</button>
+
+let transactions = dummyData;
+
+// Load initial transaction details
+function loadTransactionDetails(transaction) {
+  const sign = transaction.amount < 0 ? "-" : "+";
+  const item = document.createElement("li");
+  item.classList.add(transaction.amount < 0 ? "exp" : "inc");
+  item.innerHTML = `
+    ${transaction.description}
+    <span>${sign} ₹${Math.abs(transaction.amount)}</span>
+    <button class="btn-del" onclick="removeTransaction(${transaction.id})">x</button>
   `;
   trans.appendChild(item);
 }
 
-function removeTrans(id) {
-    if (confirm("Are u sure want to delete transaction?")) {
-      transaction = transaction.filter((transaction) => {
-        return transaction.id !== id;
-      });
-      refreshPage();
-    } else {
-      return;
-    }
-  }
-  
-function updateAmount(){
-    const amounts=transaction.map((transaction)=> transaction.amount);
-    const total=amounts.reduce((acc,item)=>(acc+=item),0).toFixed(2);
-    balance.innerHTML=` ₹ ${total}`;
+// Remove a transaction
+function removeTransaction(id) {
 
-}
-
-function refreshPage(){
-    trans.innerHTML="";
-    transaction.forEach(loadTransactionDetails);
-    updateAmount();
-}
-window.addEventListener("load", function () {
+    transactions = transactions.filter((transaction) => transaction.id !== id);
     refreshPage();
+  
+}
+
+// Update the balance, income, and expense amounts
+function updateAmount() {
+  const amounts = transactions.map((transaction) => transaction.amount);
+  const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
+  const income = amounts
+    .filter((amount) => amount > 0)
+    .reduce((acc, item) => (acc += item), 0)
+    .toFixed(2);
+  const expense = (
+    amounts.filter((amount) => amount < 0).reduce((acc, item) => (acc += item), 0) *
+    -1
+  ).toFixed(2);
+  balance.innerHTML = `₹${total}`;
+  inc_amt.innerHTML = `₹${income}`;
+  exp_amt.innerHTML = `₹${expense}`;
+}
+
+// Refresh the transaction details on the page
+function refreshPage() {
+  trans.innerHTML = "";
+  transactions.forEach(loadTransactionDetails);
+  updateAmount();
+}
+
+// Handle form submission
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  if (description.value.trim() === "" || amount.value.trim() === "") {
+    alert("Please enter a description and amount");
+    return;
+  }
+  const transaction = {
+    id: transactions.length + 1,
+    description: description.value,
+    amount: parseInt(amount.value),
+  };
+  transactions.push(transaction);
+  refreshPage();
+  description.value = "";
+  amount.value = "";
+});
+
+// Load initial transaction details and update amounts
+window.addEventListener("load", () => {
+  refreshPage();
 });
